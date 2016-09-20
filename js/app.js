9cbf45cf8bc434c25144768feba0677a6ebe8cab
jenniferjara <lisbet.jara@gmail.com>
2016-09-20 10:19:25 -0500

    segunda version

---
+++ js/app.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function() {
+	var boton = document.getElementById("boton");
+	var textArea = document.getElementById("textArea");
+	boton.addEventListener("click", function(e) {
+        e.preventDefault();//para que no aparezca en la url a pesar que es tipo submit
+		var text = textArea.value;
+		mensaje(text);
+		textArea.value = "";
+		boton.disabled = true;
+	});
+	
+	function mensaje (text) {
+        var div = document.createElement("div");
+        div.className = "nuevoMensaje";
+        var contenedor = document.getElementById("mensajes");
+        div.innerText = text;
+        contenedor.insertBefore(div,contenedor.childNodes[0])
+	}
+
+	textArea.addEventListener("keydown", function(){
+		boton.disabled = false;
+		var maxCaracteres = 140;
+		var valor = document.getElementById("textArea").value;
+		var longitud = valor.length;
+		var contador = document.getElementById("contador");
+		//contador.className = "nuevoMensaje";
+		if(longitud < maxCaracteres){
+			contador.innerText = maxCaracteres - longitud;
+		}else {
+			contador.innerText = maxCaracteres - longitud;
+		}
+
+	});
+});
--- readme.md
+++ readme.md
@@ -1,16 +1,18 @@
 #Versiones de Twitter
 
+##v0.0.1
+
 1. Diseñar un formulario que tenga un textarea y un botón.
 2. Agregar un evento de click al botón o un evento de submit al formulario.
 3. En el evento, coger el texto del textarea.
 4. Agegar el texto al HTML.
 
-##v0.0.1
+##v0.0.2
 
 1. No ingresar texto vacío (deshabilitar el botón de enviar).
 2. Contar la cantidad de caracteres de forma regresiva.
 
-##v0.0.2
+##v0.0.3
 
 1. Si pasa los 140 caracteres, deshabilitar el botón.
 2. Si pasa los 120 caracteres, mostrar el contador con OTRO color.
